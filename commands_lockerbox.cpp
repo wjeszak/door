2055beea790e8a76f0fe6ab1308f14b1f9a3e19d
wjeszak <wjeszak@gmail.com>
2018-02-03 13:57:09 +0100

    Powrót do zwracania 0xD0 przez door.

--- commands_lockerbox.cpp
+++ commands_lockerbox.cpp
@@ -19,11 +19,11 @@ void Lockerbox_CheckElmGetStatus()
 
 void Lockerbox_GetStatus()
 {
-//	if((door.lockerbox_has_been_opened && IsClosed()))
-//	{
-//		comm.Prepare(DOOR_STATE_OPENED_AND_CLOSED);
-//		return;
-//	}
+	if((door.lockerbox_has_been_opened && IsClosed()))
+	{
+		comm.Prepare(DOOR_STATE_OPENED_AND_CLOSED);
+		return;
+	}
 
 	if(IsClosed())
 		comm.Prepare(DOOR_STATE_CLOSED);
@@ -33,6 +33,7 @@ void Lockerbox_GetStatus()
 
 void Lockerbox_Open()
 {
+	door.lockerbox_has_been_opened = false;
 	comm.Prepare(0x00); // door ok
 	if(IsClosed())
 	{
